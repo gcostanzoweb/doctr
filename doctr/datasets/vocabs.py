07d75448a2423b3da5011ade414a55abcdf7295d
Gabriele Costanzo <34285858+gcostanzoweb@users.noreply.github.com>
2022-05-25 17:07:15 +0200

    Alternative: no whitespace in vocabs, exclude in encoding

--- doctr/datasets/vocabs.py
+++ doctr/datasets/vocabs.py
@@ -20,7 +20,7 @@ VOCABS: Dict[str, str] = {
     'hindi_digits': '٠١٢٣٤٥٦٧٨٩',
     'arabic_diacritics': 'ًٌٍَُِّْ',
     'arabic_punctuation': '؟؛«»—',
-    'whitespace': ' \n'
+    'whitespace': '' # for future use ' \n'
 }
 
 VOCABS['latin'] = VOCABS['digits'] + VOCABS['ascii_letters'] + VOCABS['punctuation'] + VOCABS['whitespace']
